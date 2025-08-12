e28e93d0f2bc1cbe7210c57bf3436a25537572ac
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2025-08-12 18:11:09 -0300

    PickUp & Drop / Container prefab variants

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -11,13 +11,14 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
+        if (!player.HasKitchenObject())
         {
+            // Player is not carrying anything
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
 
-            // Sets the kitchenObject field and moves the visual
+            // Sets the kitchenObject field, moves the visual and clears the previous parent
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
-        }
+        }        
     }
 }
